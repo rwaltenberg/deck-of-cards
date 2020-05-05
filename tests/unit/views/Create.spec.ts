6c9feacb22e71d0f5a9ec938669d1bdac35c78e3
Rodrigo Waltenberg <rodrigo@frametools.dev>
2020-05-05 15:18:13 -0300

    Components testing

---
+++ tests/unit/views/Create.spec.ts
@@ -0,0 +1,87 @@
+import Vuex, { Store } from 'vuex'
+import { Wrapper, createLocalVue, shallowMount } from '@vue/test-utils'
+
+import Create from '@/views/Create.vue'
+
+function setInputValue (input: Wrapper<Create>, value: string) {
+  const el = input.element as HTMLInputElement
+  el.value = value
+  input.trigger('input')
+}
+
+describe('Create.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = shallowMount(Create)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('Form submition', () => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    let actions!: { createDeck: jest.Mock<any, any>}
+    let store!: Store<any>
+
+    beforeEach(() => {
+      actions = { createDeck: jest.fn(async () => {}) }
+      store = new Vuex.Store({ actions })
+    })
+
+    it('does not trigger "save" until there is at least one card and one rotation card', () => {
+      const wrapper = shallowMount(Create, {
+        store,
+        localVue,
+        mocks: {
+          $router: { push: () => {} }
+        }
+      })
+
+      const card = wrapper.find('#card0')
+      const rotation = wrapper.find('#rotation')
+      const form = wrapper.find('form').element as HTMLFormElement
+
+      form.submit()
+      expect(actions.createDeck).not.toBeCalled()
+
+      setInputValue(card, 'AS')
+      card.trigger('input')
+
+      form.submit()
+      expect(actions.createDeck).not.toBeCalled()
+
+      setInputValue(rotation, 'AS')
+      rotation.trigger('input')
+
+      form.submit()
+      expect(actions.createDeck).toBeCalled()
+
+      expect(actions.createDeck).toHaveBeenCalledTimes(1)
+    })
+
+    it('triggers "save" only once until finished loading', () => {
+      const wrapper = shallowMount(Create, {
+        store,
+        localVue,
+        mocks: {
+          $router: { push: () => {} }
+        }
+      })
+
+      const card = wrapper.find('#card0')
+      const rotation = wrapper.find('#rotation')
+      const form = wrapper.find('form').element as HTMLFormElement
+
+      setInputValue(card, 'AS')
+      card.trigger('input')
+
+      setInputValue(rotation, 'AS')
+      rotation.trigger('input')
+
+      form.submit()
+      form.submit()
+
+      expect(actions.createDeck).toHaveBeenCalledTimes(1)
+    })
+  })
+
+})
---
+++ tests/unit/views/Deck.spec.ts
@@ -0,0 +1,102 @@
+import Vuex, { Store } from 'vuex'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+
+import Deck from '@/utils/deck'
+import DeckDetails from '@/views/Deck.vue'
+
+describe('Deck.vue', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  let mockDeckId = 'axj2sdfdc'
+  let actions!: { fetchDeck: jest.Mock<any, any>}
+  let store!: Store<any>
+  let getters: { deck: () => Deck | null }
+
+  beforeEach(() => {
+    actions = { fetchDeck: jest.fn(async () => {}) }
+    getters = {
+      deck: () => (new Deck(['2H', '2D', '2C', '2S', '3H', '3D', '3C'], '2H', mockDeckId))
+    }
+
+    store = new Vuex.Store({ actions, getters })
+  })
+
+  it('does not try to fetch if already set on storage and route id matches', () => {
+    const wrapper = shallowMount(
+      DeckDetails,
+      {
+        store,
+        localVue,
+        mocks: {
+          $route: {
+            params: {
+              id: mockDeckId
+            }
+          }
+        }
+      })
+
+    expect(actions.fetchDeck).not.toHaveBeenCalled()
+  })
+
+  it('tries to fetch deck when there is none from store', () => {
+    const emptyGetters =  {
+      deck: () => null
+    }
+
+    store = new Vuex.Store({ actions, getters: emptyGetters })
+
+    const wrapper = shallowMount(
+      DeckDetails,
+      {
+        store,
+        localVue,
+        mocks: {
+          $route: {
+            params: {
+              id: mockDeckId
+            }
+          }
+        }
+      })
+
+    expect(actions.fetchDeck).toHaveBeenCalled()
+  })
+
+  it('tries to fetch deck when deck id differs from route\'s deck id', () => {
+    const wrapper = shallowMount(
+      DeckDetails,
+      {
+        store,
+        localVue,
+        mocks: {
+          $route: {
+            params: {
+              id: '3a2s1d5a3'
+            }
+          }
+        }
+      })
+
+    expect(actions.fetchDeck).toHaveBeenCalled()
+  })
+
+  it('renders correctly', () => {
+    const wrapper = shallowMount(
+      DeckDetails,
+      {
+        store,
+        localVue,
+        mocks: {
+          $route: {
+            params: {
+              id: mockDeckId
+            }
+          }
+        }
+      })
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
