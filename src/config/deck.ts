bca252e650c6eb21b833c74c6c114ea840869cf8
Rodrigo Waltenberg <rodrigo@frametools.dev>
2020-05-05 10:44:52 -0300

    Configurations

---
+++ src/config/deck.ts
@@ -0,0 +1,7 @@
+export const Ranks = Object.freeze(['2', 'A', 'K', 'Q', 'J', '0', '9', '8', '7', '6', '5', '4', '3'] as Rank[])
+export const Suits = Object.freeze(['H', 'D', 'C', 'S'] as Suit[])
+
+export default {
+  Ranks,
+  Suits
+}
---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+declare type Rank = 'K' | 'Q' | 'J' | '10' | '9' | '8' | '7' | '6' | '5' | '4' | '3' | '2' | 'A'
+declare type Suit = 'H' | 'D' | 'C' | 'S'
+declare type Card = [Rank, Suit]
